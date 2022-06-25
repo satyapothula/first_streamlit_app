3abebb4a0aaa97069126a4d87a86d77409459b8d
SATYA SAIRAM POTHULA <55035438+satyapothula@users.noreply.github.com>
2022-06-25 18:09:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,13 +46,18 @@ streamlit.stop()
 
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+def get_fruit_load_list():
+        with my_cnx.cursor() as my cur:
+                my_cur.execute("Select * from fruit_load_list")
+                return my_cur.fetchall()
 
+        
+if streamlit.button('Get Fruit Load List'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        my_data_rows = my_cur.fetchall()
+        streamlit.dataframe(my_data_rows)
+        
+        
 def insert_row_snowflake(new_fruit):
         with my_cnx.cursor() as my_cur:
                 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
